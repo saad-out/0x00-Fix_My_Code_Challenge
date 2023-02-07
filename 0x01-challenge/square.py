99818560fda200c2d587a3435c85ef1f44b970a0
saad-out <outsaad03@gmail.com>
2023-02-07 08:59:01 +0000

    use one naming style for functions & correct area calculation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,22 +2,27 @@
 
 class square():
     
-    def __init__(self, size):
-        self.size =  size
+    width = 0
+    height = 0
+
+    
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.size ** 2
+        return self.height * self.width
 
-    def PermiterOfMySquare(self):
-        return self.size * 4
+    def permiter_of_my_square(self):
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}".format(self.size)
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(12)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
